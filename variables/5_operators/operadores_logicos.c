46287a9f582a7c0844772f4872ec2014dd227413
Mónica Ruiz R <monru.z05@gmail.com>
2020-11-19 00:20:38 +0000

    Creación de archivo puts, desarrollo de ejercicio de área de circulo.

--- variables/5_operators/operadores_logicos.c
+++ variables/5_operators/operadores_logicos.c
@@ -1,19 +1,23 @@
 #include <stdio.h> 
 #include <stdlib.h>
-
+/*
+|| OR
+&& AND
+! NOT
+*/
 int main()
 { // Inicio
 
  system("clear"); // Limpia pantalla
  
- printf("!(1 != 1) devuelve %d que implica verdadero\n",!(1 != 1));
- printf("!(1 == 1) devuelve %d que implica falso\n",!(1 == 1));
- printf("1 == 1 && 2 == 2 devuelve %d que implica verdadero\n",1 == 1 && 2 == 2);
- printf("1 == 1 && 2 == 3 devuelve %d que implica falso\n",1 == 1 && 2 == 3);
- printf("1 == 1 || 2 == 3 devuelve %d que implica verdadero\n",1 == 1 || 2 == 3);
- printf("1 != 1 || 2 == 3 devuelve %d que implica falso\n",1 != 1 || 2 == 3);
- printf("1 >= 1 || 2 <= 3 devuelve %d que implica verdadero\n",1 >= 1 || 2 <= 3);
- printf("1 >= 1 || 2 >= 3 devuelve %d que implica verdadero\n",1 >= 1 || 2 >= 3);
+ printf("!(1 != 1) devuelve %d que implica verdadero\n",!(1 != 1)); // !(0) =1
+ printf("!(1 == 1) devuelve %d que implica falso\n",!(1 == 1)); //!(1) = 0
+ printf("1 == 1 && 2 == 2 devuelve %d que implica verdadero\n",1 == 1 && 2 == 2);// 1 && 1 = 1
+ printf("1 == 1 && 2 == 3 devuelve %d que implica falso\n",1 == 1 && 2 == 3);// 1 && 0 = 0
+ printf("1 == 1 || 2 == 3 devuelve %d que implica verdadero\n",1 == 1 || 2 == 3); // 1 || 0 = 1
+ printf("1 != 1 || 2 == 3 devuelve %d que implica falso\n",1 != 1 || 2 == 3); // 0 || 0 = 0
+ printf("1 >= 1 || 2 <= 3 devuelve %d que implica verdadero\n",1 >= 1 || 2 <= 3); //1 || 1 = 1
+ printf("1 >= 1 || 2 >= 3 devuelve %d que implica verdadero\n",1 >= 1 || 2 >= 3); // 1 || 0 = 1
  printf("'A' == 'A' devuelve %d que implica verdadero\n",('A' == 'A'));
  printf("'A' > 'B' devuelve %d que implica falso\n",('A' > 'B'));
  printf("'A' < 'B' devuelve %d que implica verdadero\n",('A' < 'B'));
--- variables/6_read/scanf.c
+++ variables/6_read/scanf.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
+#include <math.h>
 #define PI 3.14159
 int main()
 {
 int r;
-float l, a;
+float d, a;
 printf("Introduce radio (entero): ");
 scanf("%d", &r);
-l=2*PI*r; 
-a=PI*r*r;
-printf("La longitud de la circunferencia vale %f\n", l);
-printf("El área del círculo vale %f\n", a);
+d=(2*PI)*r; // Formula de diametro
+a=PI*pow(r,2) ; // Formula de area de circulo
+printf("El diametro de la circunferencia vale %0.2f\n", d);
+printf("El área del círculo vale %0.2f\n", a);
 }
